bef5f6fdbc5ec8390896bbeffdc3d1fc06a47646
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-18 20:59:07 +0700

    commit

---
+++ exercise-2.js
@@ -0,0 +1,46 @@
+var nama = '';
+var peran = '';
+
+console.log ('Halo [nama], pilih peranmu untuk memulai game');
+if(nama == '') {
+    console.log("nama tidak diisi");
+  }
+  else {
+      console.log("Nama harus diisi bos!");
+    }
+
+var nama ='Danang';
+var peran='';
+ if(nama == Danang){
+    console.log("Halo Danang, Pilih peranmu untuk memulai game!");
+ }
+else {
+    console.log("Nama harus diisi bos!");
+}
+var nama ='Broto';
+var peran='ksatria';
+if (nama == 'Broto');{
+    console.log("Selamat datang di Dunia Proxytia, Broto"
+    "Halo Ksatria Broto, kamu dapat menyerang dengan senjatamu!");
+}
+ else {
+    console.log("Nama harus diisi bos!");
+ }
+var nama ='Jing';
+var peran='Tabib';
+if(nama=='jing');{
+    "Selamat datang di Dunia Proxytia, Jing"
+"Halo Tabib Jing, kamu akan membantu temanmu yang terluka."
+}
+ else {
+    console.log("Nama harus diisi bos!");
+}
+var nama='Rosalinda';
+var peran='pemyihir';
+if(nama=='Rosalinda');{
+    "Selamat datang di Dunia Proxytia, Rosalinda"
+"Halo Penyihir Rosalinda, ciptakan keajaiban yang membantu kemenanganmu."
+}
+ else {
+    console.log("Nama harus diisi bos!");
+}
