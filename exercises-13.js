27f1b9c8c54b9d318be86e023f6278e624d1f406
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-20 23:24:22 +0700

    commit

---
+++ exercises-13.js
@@ -0,0 +1,18 @@
+function xo(str) {
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === 'x') {
+            x++;
+        }
+        if(str[i] === 'o'){
+            o++;
+        }
+    }
+    return x !== o ? false : true;
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
