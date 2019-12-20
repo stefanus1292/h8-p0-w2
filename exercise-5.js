5301d68ff3e8d18025d0c10e0e48eecfb4353325
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-20 21:10:26 +0700

    commit

---
+++ exercise-5.js
@@ -0,0 +1,52 @@
+
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+console.log('JavaScript'+'Is'+'awesome'+'and'+ 'I'+ 'Love'+'It!'); //JavaScript is awesome and I love it!
+
+
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord; word[0] + word [1] + word [2];
+var thirdWord; word [1] + word [2] + word [3];
+var fourthWord; word [2] + word [3] + word [4];
+var fifthWord; word [3] + word [4] + word[5];
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3; word.substring(0,2);
+var thirdWord3; word.substring(0,4);
+var fourthWord3; word.substring(0,5);
+var fifthWord3; word.substring(2,3);
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4;word.substring(1,2);
+var thirdWord4;word.substring(2,3)
+var fourthWord4;word.substring(3,4);
+var fifthWord4;word.substring(4,5);
+
+var firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
