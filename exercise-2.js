85a55cf23640a0460348b2f3d2191dd06890de12
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-20 14:32:32 +0700

    commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,46 +1,8 @@
-var nama = '';
-var peran = '';
-
-console.log ('Halo [nama], pilih peranmu untuk memulai game');
-if(nama == '') {
-    console.log("nama tidak diisi");
-  }
-  else {
-      console.log("Nama harus diisi bos!");
-    }
-
-var nama ='Danang';
-var peran='';
- if(nama == Danang){
-    console.log("Halo Danang, Pilih peranmu untuk memulai game!");
- }
-else {
-    console.log("Nama harus diisi bos!");
-}
-var nama ='Broto';
-var peran='ksatria';
-if (nama == 'Broto');{
-    console.log("Selamat datang di Dunia Proxytia, Broto"
-    console.log(`Halo Ksatria Broto, kamu dapat menyerang dengan senjatamu!`${peran});
-}
- else {
-    console.log("Nama harus diisi bos!");
- }
-var nama ='Jing';
-var peran='Tabib';
-if(nama=='jing');{
-    console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
-"Halo Tabib Jing, kamu akan membantu temanmu yang terluka."
-}
- else {
-    console.log("Nama harus diisi bos!");
-}
-var nama='Rosalinda';
-var peran='pemyihir';
-if(nama=='Rosalinda');{
-    "Selamat datang di Dunia Proxytia, Rosalinda"
-"Halo Penyihir Rosalinda, ciptakan keajaiban yang membantu kemenanganmu."
-}
- else {
-    console.log("Nama harus diisi bos!");
+var nama =Danang ;
+var peran=Ksatria ;
+if(peran == 'ksatria' + 'tabib' + 'penyihir'); {
+  console.log("Halo Ksatria [nama] , kamu dapat menyerang dengan senjatamu!");
+  console.log("Halo Tabib [nama], kamu akan membantu temanmu yang terluka.");
+  console,log("Halo Penyihir [nama], ciptakan keajaiban yang membantu kemenanganmu!"
+  );
 }
