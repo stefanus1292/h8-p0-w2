47035dcadae43e877c27a393146453599e91616e
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-20 22:30:47 +0700

    commit

---
+++ exercises-9.js
@@ -0,0 +1,23 @@
+function tampilkan() {
+    console.log(shoutOut("Halo Function!"));
+  }
+  
+  tampilkan();
+
+var num1 = 6;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2)
+    return num1*num2
+
+console.log(hasilPerkalian(6,6));
+
+
+
+var name = "Tono";
+var age = 29;
+var address = "Jln. jalan, Malioboro, Yogyakarta";
+var hobby = "Memancing";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(""Nama saya [Name], umur saya [Age] tahun, alamat saya di [Address], dan saya punya hobby yaitu [hobby]!""); 
