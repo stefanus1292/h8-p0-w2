df3563ac93dc6e23741d2baa8632488937ea4ce4
stefanus1292 <gunawanstefanus12@gmail.com>
2019-12-20 22:07:12 +0700

    commit

---
+++ exercises-6.js
@@ -0,0 +1,28 @@
+
+var word = 2;
+while(word < 30) { 
+  console.log('i love coding'+2); 
+  word++;}
+
+  var word = 2;
+while(word < 20) { 
+  console.log('I will become fullstack developer'-2); 
+  word--;}
+
+  for(var angka = 1; angka <= 20; angka++) {
+    console.log('i love coding' + angka);
+  }
+
+  for(var angka = 1; angka <= 20; angka--) {
+    console.log('I will become fullstack developer' - angka);
+  }
+
+  for(var deret = 0; deret < 10; deret += 2) {
+    console.log('Iterasi dengan Increment counter 2: ' + deret);
+  }
+  
+  console.log('-------------------------------');
+  
+  for(var deret = 6; deret > 0; deret -= 3) {
+    console.log('Iterasi dengan Decrement counter : ' + deret);
+  }
